360f258488f9c3c07ddec3692bb7f9fbd67f405f
Misael3101 <torres.31012407@gmail.com>
2025-06-29 14:39:00 -0300

    modificaciones en el home, ahora funciona con redux

--- pv_tpint_grupo6/src/pages/Home.jsx
+++ pv_tpint_grupo6/src/pages/Home.jsx
@@ -1,11 +1,14 @@
-import { useContext, useEffect } from "react";
-import { AppContext } from "../context/AppContext";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import ProductCard from "../components/ProductCard";
 
 const Home = () => {
-  const { products, setProducts, toggleFavorite } = useContext(AppContext);
+  const dispatch = useDispatch();
+  const products = useSelector(state => state.products.products) || [];
 
-  useEffect(() => {
+
+useEffect(() => {
+  if (products.length === 0) {
     const fetchProducts = async () => {
       try {
         const res = await fetch("https://fakestoreapi.com/products");
@@ -14,14 +17,19 @@ const Home = () => {
           ...product,
           isFavorite: false
         }));
-        setProducts(productsWithFavorites);
+        dispatch({ type: "SET_PRODUCTS", payload: productsWithFavorites });
       } catch (error) {
         console.error("Error fetching products:", error);
       }
     };
 
     fetchProducts();
-  }, [setProducts]);
+  }
+}, [dispatch, products.length])
+
+  const toggleFavorite = (id) => {
+    dispatch({ type: "TOGGLE_FAVORITE", payload: id });
+  };
 
   return (
     <div className="home">
