1af069efd5a5cd2310365fc535fa5512f969da97
Misael3101 <torres.31012407@gmail.com>
2025-06-29 14:33:27 -0300

    modificaciones en el app para tener rutar privadas

--- pv_tpint_grupo6/src/App.jsx
+++ pv_tpint_grupo6/src/App.jsx
@@ -1,22 +1,52 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { AppProvider } from "./context/AppContext";
-import NavBar from './components/NavBar';
+import NavBar from "./components/NavBar";
 import Home from "./pages/Home";
-import Favorites from './pages/Favorites';
-import "./App.css";
+import Favorites from "./pages/Favorites";
+import ProductDetail from "./pages/productDetails";
+import ProductForm from "./pages/productForm";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import PrivateRoute from "./components/PrivateRoute";
+import './App.css';
 
 function App() {
   return (
-    <AppProvider>
-      <Router>
-        <NavBar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-           <Route path="/favorites" element={<Favorites />} />
-        </Routes>
-      </Router>
-    </AppProvider>
+    <Router>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route
+          path="/favorites"
+          element={
+            <PrivateRoute>
+              <Favorites />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/product/new"
+          element={
+            <PrivateRoute>
+              <ProductForm />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/product/edit/:id"
+          element={
+            <PrivateRoute>
+              <ProductForm />
+            </PrivateRoute>
+          }
+        />
+        <Route path="/product/:id" element={<ProductDetail />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </Router>
   );
 }
 
 export default App;
+
+
