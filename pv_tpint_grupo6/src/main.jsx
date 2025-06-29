9415e62439167284024cb52fee5c2c12b64a5d37
Misael3101 <torres.31012407@gmail.com>
2025-06-29 14:44:29 -0300

    main-redux

--- pv_tpint_grupo6/src/main.jsx
+++ pv_tpint_grupo6/src/main.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from "react-redux";
+import store from './store/store.js';
 import App from './App.jsx';
-import { AppProvider } from './context/AppContext.jsx';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <AppProvider>
+    <Provider store={store}>
       <App />
-    </AppProvider>
+    </Provider>
   </React.StrictMode>
 );
